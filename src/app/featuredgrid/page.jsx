5550cf7d03fb9e5dd2da31443c239e925770d4b7
a-terohid <87315139+a-terohid@users.noreply.github.com>
2026-03-17 12:55:15 -0400

    update

--- src/app/featuredgrid/page.jsx
+++ src/app/featuredgrid/page.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const page = () => {
     return (
         <div className=" container pb-12 ">
-            <div id="rfWidgets" config= "featured-listings-2" widget="FeaturedListings"></div>
+            <div id="rfWidgets" config="featured-listings-2" widget="FeaturedListings"></div>
         </div>
     );
 };
